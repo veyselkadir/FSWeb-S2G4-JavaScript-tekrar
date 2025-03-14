29065da7483b0b19700f8651867d1bf53413900d
veyselkadir <veysel.kadir56@gmail.com>
2025-03-14 11:50:56 +0300

    yenilendi

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ const sayilar = [
 function KareninAlani(kenaruzunlugu) {
   return kenaruzunlugu * kenaruzunlugu;
 }
+console.log(kenaruzunlugu(10))
 
 /* (Oto test yok) Yukarıdaki KareninAlani fonksiyonunu kenar uzunluğu = 10 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
